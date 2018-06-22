13f93d6992357a1de765134773a19592369203ef
Rickard31 <vitalka.marchenko@gmail.com>
2018-06-23 00:40:07 +0300

    Homework 5: added Main Menu Scene

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	private static String levelSelectionName = "LevelSelection";
+	public void goToLevelSelection()
+	{
+		SceneManager.LoadScene(levelSelectionName);
+	}
+
+
+	private static String level1Name = "Level1";
+	public void GoToLevel1()
+	{
+		//Debug.Log("GoToLevel1");
+		SceneManager.LoadScene(level1Name);
+	}
+	
+	private static String level2Name = "Level2";
+	public void GoToLevel2()
+	{
+		//Debug.Log("GoToLevel2");
+		SceneManager.LoadScene(level2Name);
+	}
+}
