082adad26eff567b7339ccacfaadf40af200f149
Rickard31 <vitalka.marchenko@gmail.com>
2018-06-25 17:26:14 +0300

    Homework 7: Added background music

---
+++ Assets/Scripts/BackgroundMusicController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusicController : MonoBehaviour {
+	
+	public AudioClip music = null;
+	AudioSource musicSource = null;
+
+	// Use this for initialization
+	void Start () {
+		musicSource = gameObject.AddComponent<AudioSource>();
+		musicSource.clip = music;
+		musicSource.loop = true;
+		musicSource.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
